7931e2aaed78ab9b15a057cc49d9efe1fedec8bc
EagleBoiXD <63115595+EagleBoiXD@users.noreply.github.com>
2023-06-28 15:12:14 +0300

    bistabil corect + merge wave yippee
    adaugare de pl la bistabil nu il mai face bistabil

--- Tema1/Siso.v
+++ Tema1/Siso.v
@@ -1,54 +1,49 @@
-module topSISO;//nu merge
+module topSISO;//merge acum
     reg[3:0]a;
     wire si,d0,d1,d2,dout;
     reg clk,reset,pl;
     SerialIn Sisul(a,pl,si);
-    BistabilD D0(si,pl,clk,reset,d0);
-    BistabilD D1(d0,pl,clk,reset,d1);
-    BistabilD D2(d1,pl,clk,reset,d2);
-    BistabilD D3(d2,pl,clk,reset,dout);
+    BistabilD D0(si,clk,reset,d0);
+    BistabilD D1(d0,clk,reset,d1);
+    BistabilD D2(d1,clk,reset,d2);
+    BistabilD D3(d2,clk,reset,dout);
     initial begin
-        a=0;reset=0;clk=0;pl=0;
-        
-        #10
-        a=5;
-        #4
-        pl=1;
-        #5 
-        pl=0;
-        #5
-        pl=1;
-        #5
-        pl=0;
-        #5
-        pl=1;
-        #5
-        pl=0;
-        #5
-        pl=1;
-        #5
-        pl=0;
-        #5
-        pl=1;
-        #5
-        pl=0;
+        a=0;reset=0;clk=0;
+        #10 a=6;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
+        #10 pl=1;
+        #10 pl=0;
         #100
         $finish();
-        forever #10 clk=~clk;
+        
     end
-    
+    initial forever #10 clk=~clk;
     
 endmodule
 
-module BistabilD(A,pl,clk,reset,out);
-  input A,clk,reset,pl;
+module BistabilD(A,clk,reset,out);
+  input A,clk,reset;
   output reg out;
   
   always@(posedge clk)begin
     if(reset==1)
       out=0;
     else 
-    if(pl==1)
       out<=A;
   end
  	

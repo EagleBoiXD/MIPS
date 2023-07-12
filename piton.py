c502c82201b87ccbedfdacdedbf8767da51333cc
EagleBoiXD <63115595+EagleBoiXD@users.noreply.github.com>
2023-07-12 16:21:45 +0300

    dictionar?????????????

--- piton.py
+++ piton.py
@@ -1,9 +1,12 @@
+import re
+
 def instr(line): #line = toata instr
     opcode=rest=""
     line = line.split(' ',1)
     op = line[0]
     rest = line[1]
-    
+    # POTI FACE HASHMAP (DEFAPT DICTIONAR) 
+    # also cauta ce e aia dictionar in piton
     
     if op == 'addi':
         opcode = '001000'
@@ -14,7 +17,7 @@ def instr(line): #line = toata instr
     if op == 'sw':
         opcode = '101011'
         print(opcode)
-    if op == 'xor' or op == 'add' or op == 'sub':
+    if op == 'xor' or op == 'add' or op == 'sub' or op == 'and':
         opcode = '000000'
         print(opcode)
     if op == 'beq':
